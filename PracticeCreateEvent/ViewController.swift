3467ed8bfd3cbda2d485d1bed4fc3f26e3ae41f2
Justin Smith <11jsmith7@gmail.com>
2016-08-02 10:00:29 -0600

    initial commit
    Signed-off-by: Justin Smith <11jsmith7@gmail.com>

--- PracticeCreateEvent/ViewController.swift
+++ PracticeCreateEvent/ViewController.swift
@@ -8,18 +8,71 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        setupTableViewUI()
+    }
+    
+    func setupTableViewUI() {
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 0.278, green: 0.310, blue: 0.310, alpha: 1.00)
+        self.navigationController?.navigationBar.tintColor = UIColor(red: 0.000, green: 0.663, blue: 0.800, alpha: 1.00)
+        //self.tableView.separatorColor = .clearColor()
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : UIColor.whiteColor()]
+        setBackgroundForTableView()
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
+    
+    func setBackgroundForTableView() {
+        
+        let blurEffect = UIBlurEffect(style: .Light)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        
+        let imageView = UIImageView(image: UIImage(named: "Turnn Background")!)
+        imageView.contentMode = .Center
+        
+        imageView.addSubview(blurView)
+        tableView.backgroundView = imageView
+        blurView.frame = imageView.frame
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK - TableView Datasource
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = .clearColor()
+        return cell
+    }
+    
+    // MARK - TableView Delegate
 
-
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    
 }
 
